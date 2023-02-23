f71f0a21ed2d26782010f6bfdb7607a45ef927c8
Aramburu1011 <aramburu1011@hotmail.com>
2023-02-23 09:23:58 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send('HOLAa');
+})
+app.get('/adios',(req, res)=>{
+    res.send('ADIOS');
+    console.log("adios")
+})
+
+app.listen(3000, (req, res)=>{
+    console.log('Servidor corriendo en https://localhost:3000');
+})
