64bb8742a0516877a595d59cbee990c9758c34c8
Aramburu1011 <aramburu1011@hotmail.com>
2023-02-26 20:48:14 +0100

    Inicio aplicacion

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bcryptjs = require('bcryptjs');
+const session = require('express-session');
+const connection = require('./database/db');
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+dotenv.config({path:'./env/.env'});
+
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret:'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
 app.get('/',(req, res)=>{
-    res.send('HOLA');
-    console.log("hola")
-})
-app.get('/adios',(req, res)=>{
-    res.send('ADIOS');
-    console.log("adios")
+    res.render('index');
 })
 
 app.listen(3000, (req, res)=>{
