513220dbb216890eb09dd8fae10eec649a57fe97
Aramburu1011 <aramburu1011@hotmail.com>
2023-05-22 20:54:13 +0200

    Nueva web

--- controllers/menu/index.js
+++ controllers/menu/index.js
@@ -12,7 +12,40 @@ const menu = async (peticion, respuesta) => {
 		console.log (error.message)
 	}
 }
+const about = async (peticion, respuesta) => {
+	try {
+		
+
+		respuesta.render ('../views/about', {
+		})
+	} catch (error) {
+		console.log (error.message)
+	}
+}
+const jobs = async (peticion, respuesta) => {
+	try {
+		
+
+		respuesta.render ('../views/job_listing', {
+		})
+	} catch (error) {
+		console.log (error.message)
+	}
+}
+const contact = async (peticion, respuesta) => {
+	try {
+		
+
+		respuesta.render ('../views/contact', {
+		})
+	} catch (error) {
+		console.log (error.message)
+	}
+}
 
 module.exports = {
-    menu: menu
+    menu: menu,
+	about: about,
+	jobs: jobs,
+	contact: contact
 }
--- routes/index/menu.js
+++ routes/index/menu.js
@@ -2,5 +2,9 @@ const router = require ('express').Router ()
 const menu_functions = require ('../../controllers/menu')
 
 router.get ('/', menu_functions.menu)
+router.get ('/about', menu_functions.about)
+router.get ('/job_listing', menu_functions.jobs)
+router.get ('/contact', menu_functions.contact)
+
 
 module.exports.router = router
